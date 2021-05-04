9829923ff28ab96a53e713240092c33588a4c81d
Gayathrimenakath <gayathrimenakath@gmail.com>
2021-05-04 23:44:34 +0200

    Changing the data format

--- piechart.py
+++ piechart.py
@@ -6,34 +6,55 @@ from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import *
 
-
+sample = {
+   "Alloys":{
+      "aluminium":25,
+      "Copper": 75
+    },
+    "aluminium":{
+      "Duralumin": 20,
+      "Hydronalium":30,
+      "magnaliuim": 50,     
+    },
+    "magnaliuim":{
+      "aluminium":45,
+      "magnesium": 55
+    },
+    "Duralumin":{
+       "copper": 50,
+       "aluminium":50
+    },
+    "Hydronalium":{
+       "manganese": 1,
+       "magnesium": 12,
+       "aluminium": 87
+    },
+    "Copper":{
+       "Beryllium copper": 30,
+       "Billon": 45,
+       "Copper–tungsten" :35
+    },
+    "Beryllium copper":{
+       "Beryllium": 3,
+       "copper":97
+    },
+    "Billon":{
+       "gold": 13,
+       "copper": 87
+    },
+    "Copper–tungsten":{
+       "Tungsten": 40,
+       "copper":60
+    }
+  }
 
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
-        thisDict =  {
-      "Protein": 4.2,
-      "Fat": 15.6,
-      "carbs": 23.8,
-      "other": 56.4
-   }
-   
-        self.thisDict =  {
-      "Protein": {
-      "carbs": 43.6,
-      "other": 56.4
-   },
-      "Fat": {
-      "calories": 43.6,
-      "other": 56.4
-   },
-      "carbs": 23.8,
-      "other": 56.4
-   }
-   
+       
         self.setWindowTitle("DonutChart Example")
         self.setGeometry(100,100, 400,600)
-        self.create_donutchart(thisDict)
+        self.create_donutchart('Alloys')
         self.show()
        
     @pyqtSlot()
@@ -42,11 +63,11 @@ class Window(QMainWindow):
         self.labl.adjustSize()
         
 
-    def create_donutchart(self, thisDict):
+    def create_donutchart(self, chartKey):
         series = QPieSeries()
         series.setHoleSize(0.35)
         
-        for key, value in thisDict.items():
+        for key, value in sample[chartKey].items():
             series.append(key, value)
        
 
@@ -77,14 +98,15 @@ class Window(QMainWindow):
         
         series.doubleClicked.connect(self.handle_double_clicked)
         
+        
     #Show the update chart with the distribution of the selected slice
     def handle_double_clicked(self, slice):
         slice.setExploded()
         slice.setLabelVisible()
         
-        if slice.label() in self.thisDict:
+        if slice.label() in sample:
             #print(slice.series().label());
-            self.create_donutchart(self.thisDict[slice.label()])
+            self.create_donutchart(slice.label())
    
 
 
