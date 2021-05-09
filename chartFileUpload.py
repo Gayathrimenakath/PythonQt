5460f0231ec6bda90ba726d9e2e0c6df30e3051f
Gayathrimenakath <gayathrimenakath@gmail.com>
2021-05-09 14:22:19 +0200

    Removed unnecessary comments

--- chartFileUpload.py
+++ chartFileUpload.py
@@ -28,7 +28,6 @@ class Chart(QWidget):
 
         self.chartview = QChartView(self.chart)
         self.chartview.setRenderHint(QPainter.Antialiasing)
-       
         
         
     def addSeries(self, key):
@@ -44,7 +43,6 @@ class Chart(QWidget):
         self.chart.addSeries(self.series)
         self.series.doubleClicked.connect(self.handle_double_clicked)
              
-        
           
     #Show the update chart with the distribution of the selected slice
     def handle_double_clicked(self, slice):
@@ -60,20 +58,16 @@ class HomeScreen(QtCore.QObject):
     def __init__(self, a, parent=None):
         super(HomeScreen, self).__init__(parent)
         self.centralwidget = QtWidgets.QWidget()
-        #self.centralwidget.setObjectName("centralwidget")
         
         self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
-        #self.gridLayout = QtWidgets.QGridLayout()
         self.gridLayout.setObjectName("gridLayout")
         
-        #self.label1 = QtWidgets.QLabel(self.centralwidget)
         self.label1 = QtWidgets.QLabel()
         self.label1.setText('Welcome to PyQt5 example')
         self.label1.setAutoFillBackground(True)
         self.label1.setFont(QtGui.QFont("Times", 20,weight=QtGui.QFont.Bold))
         self.label1.setAlignment(Qt.AlignCenter)
         
-        #self.label3 = QtWidgets.QLabel(self.centralwidget)
         self.label3 = QtWidgets.QLabel()
         self.label3.setGeometry(10, 210, 500, 23)
         self.label3.setObjectName("label3")
@@ -92,60 +86,32 @@ class HomeScreen(QtCore.QObject):
         self.label_2.setObjectName("label_2")
         self.gridLayout2.addWidget(self.label_2, 0, 0, 0, 1)
         
-        
-        
-        
         self.lineEdit = QtWidgets.QLineEdit(self.frame)
         self.lineEdit.setObjectName("lineEdit")
         self.gridLayout2.addWidget(self.lineEdit, 0, 1, 1, 1)
         self.pushButton = QtWidgets.QPushButton(self.frame)
         self.pushButton.setObjectName("pushButton")
-        #self.pushButton.clicked.connect(self.file_open)
         self.gridLayout2.addWidget(self.pushButton, 0, 2, 1, 1)
         
         self.gridLayout.addLayout(self.gridLayout2, 0, 0, 7, 2)
         self.gridLayout.addLayout(self.gridLayout1, 0, 0, 1, 2)
-        #self.gridLayout.addLayout(self.gridLayout3, 4, 0, 2, 0)
         
-        #MainWindow.setCentralWidget(self.centralwidget)
-        
-       
-        #self.retranslateUi(MainWindow)
-        #MainWindow.setWindowTitle( "MainWindow")
         self.label_2.setText("File Name")
         self.pushButton.setText( "Browse")
         
-        
-        #QtCore.QMetaObject.connectSlotsByName(MainWindow)
-        
-    
-    
-    
-    def showChart(self):
-        self.next_page()
-        
-            
-    
-        
+           
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
-        #MainWindow.setObjectName("MainWindow")
-        #MainWindow.resize(514, 381)
         
         self.btn_Action = QtWidgets.QPushButton()
         self.btn_Action.setGeometry(QtCore.QRect(220, 200, 75, 23))
         self.btn_Action.setObjectName("btn_Action")
         self.gridLayout3 = QtWidgets.QGridLayout()
         self.gridLayout3.addWidget(self.btn_Action, 0, 3, 3, 3)
-        #json_object = json.dumps(sampleData, indent = 4)  
-        #print(json_object) 
         
-        
-        #self.btn_Action.setText(_translate("MainWindow", "View Chart"))
-        self.btn_Action.setText("View Chart")
-        
+        self.btn_Action.setText("View Chart")        
         
         self.stacked_widget = QStackedWidget()
         self.stacked_widget.currentChanged.connect(self.set_button_state)
@@ -153,45 +119,31 @@ class MainWindow(QtWidgets.QMainWindow):
         
         widget = QtWidgets.QWidget()
         self.setCentralWidget(widget)
-
-        
-        
+     
         hbox = QHBoxLayout()
         hbox.addStretch(1)
         hbox.addWidget(self.btn_Action)
-        #hbox.addWidget(self.next_button)
         
         vbox = QVBoxLayout()
         vbox.addWidget(self.stacked_widget)
         vbox.addLayout(hbox)
         
         h = HomeScreen(self)
-        self.name  = None
         h.pushButton.clicked.connect(self.file_open)
-        print('yes')
         self.insert_page(h.centralwidget)
-        
-        
-        
+          
         # create main layout
         widget.setLayout(vbox)
 
-    #def retranslateUi(self, MainWindow):
-       # _translate = QtCore.QCoreApplication.translate
         
     def file_open(self):
-        print('called')
         name, _ = QtWidgets.QFileDialog.getOpenFileName(None, 'Open File', options=QtWidgets.QFileDialog.DontUseNativeDialog)
         file = open(name, 'r') 
         with file as json_file:
             text = json.load(json_file)
-            #text = file.read()
-            #self.lineEdit.setText(name)
-            #print(text)
             chart = Chart('Alloys', text, self)
             self.insert_page(chart.chartview)
-        
-        
+          
         
     def set_button_state(self, index):
         #self.prev_button.setEnabled(index > 0)
@@ -218,3 +170,4 @@ if __name__ == '__main__':
     w.resize(400,300)
     w.show()
     sys.exit(app.exec_())
+    
