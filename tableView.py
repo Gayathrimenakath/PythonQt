740c635a6e15e09316aad02b477943562a67559f
Gayathrimenakath <gayathrimenakath@gmail.com>
2021-05-11 23:59:41 +0200

    Added code for showing contents in table view

---
+++ tableView.py
@@ -0,0 +1,54 @@
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QAction, QTableWidget,QTableWidgetItem,QVBoxLayout
+from PyQt5.QtGui import QIcon
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import pyqtSlot
+import sys
+
+
+data = {'India':['Trivandrum', 'New Delhi','Mumbai','Bangalore','Kochi'],
+        'America':['Los Angeles','California','Arizona','New York', 'Boston'],
+        'France':['Paris','Lyon','Marseille','Bordeaux', 'Nantes'],
+        'Italy':['Milan','Rome','Genoa','Turin', 'Naples']}
+ 
+ 
+class TableView(QTableWidget):
+    def __init__(self, data, *args):
+        QTableWidget.__init__(self, *args)
+        self.data = data
+        self.setData()
+        
+    def setData(self): 
+        horHeaders = []
+        for n, key in enumerate(sorted(self.data.keys())):
+            horHeaders.append(key)
+            for m, item in enumerate(self.data[key]):
+                newitem = QTableWidgetItem(item)
+                self.setItem(m, n, newitem)
+        self.setHorizontalHeaderLabels(horHeaders)
+        
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+
+        widget = QtWidgets.QWidget()
+        self.setCentralWidget(widget)
+        lay = QtWidgets.QHBoxLayout(widget)
+        
+        table = TableView(data, 5, 4)
+        lay.addWidget(table)
+
+        # create main layout
+        widget.setLayout(lay)
+
+ 
+ 
+if __name__ == '__main__':
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    w = MainWindow()
+    w.resize(500,400)
+    w.show()
+    sys.exit(app.exec_())
+    
